a818cf15128cd023924f6dbd837531afb6a06f93
devOrdinai <gudeliuberniokas@gmail.com>
2025-07-05 18:36:35 +0100

    Initial Commit

---
+++ src/client/java/org/ordinai/ordinaiCore/client/OrdinaiCoreClient.java
@@ -0,0 +1,10 @@
+package org.ordinai.ordinaiCore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OrdinaiCoreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/ordinai/ordinaiCore/OrdinaiCore.java
@@ -0,0 +1,10 @@
+package org.ordinai.ordinaiCore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OrdinaiCore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
